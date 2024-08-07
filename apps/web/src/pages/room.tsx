6203efa6625633f25488a67a60112b4639cd4a6d
Victor Guedes <victorfelipe.guedes@hotmail.com>
2024-08-07 12:48:25 -0300

    feat(web): create message component

--- apps/web/src/pages/room.tsx
+++ apps/web/src/pages/room.tsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
-import { ArrowRight, ArrowUp, Share2 } from "lucide-react";
+import { ArrowRight, Share2 } from "lucide-react";
 import { toast } from "sonner";
+
+import { Message } from "../components/message";
 
 export function Room() {
   const { roomId } = useParams();
@@ -56,76 +58,41 @@ export function Room() {
       </form>
 
       <ol className="list-decimal list-outside px-3 space-y-8">
-        <li className="ml-4 leading-relaxed text-zinc-100">
-          O que é GoLang e quais são suas principais vantagens em comparação com
-          outras linguagens de programação como Python, Java ou C++?
-          <button
-            type="button"
-            className="mt-3  flex items-center gap-2 text-zinc-400 text-sm font-medium hover:text-zinc-300"
-          >
-            <ArrowUp className="size-4" />
-            Curtir pergunta (123)
-          </button>
-        </li>
-        <li className="ml-4 leading-relaxed text-zinc-100">
-          Como funcionam as goroutines em GoLang e por que elas são importantes
-          para a concorrência e paralelismo?
-          <button
-            type="button"
-            className="mt-3  flex items-center gap-2 text-orange-400 text-sm font-medium hover:text-orange-500"
-          >
-            <ArrowUp className="size-4" />
-            Curtir pergunta (182)
-          </button>
-        </li>
-        <li className="ml-4 leading-relaxed text-zinc-100">
-          Quais são as melhores práticas para organizar o código em um projeto
-          GoLang, incluindo pacotes, módulos e a estrutura de diretórios?
-          <button
-            type="button"
-            className="mt-3  flex items-center gap-2 text-zinc-400 text-sm font-medium hover:text-zinc-300"
-          >
-            <ArrowUp className="size-4" />
-            Curtir pergunta (123)
-          </button>
-        </li>
-        <li className="ml-4 leading-relaxed text-zinc-100">
-          Como fazer a depuração de programas GoLang e quais ferramentas são
-          recomendadas para isso?
-          <button
-            type="button"
-            className="mt-3  flex items-center gap-2 text-orange-400 text-sm font-medium hover:text-orange-500"
-          >
-            <ArrowUp className="size-4" />
-            Curtir pergunta (182)
-          </button>
-        </li>
-        <li className="ml-4 leading-relaxed text-zinc-100">
-          Quais são os primeiros passos para começar a programar em GoLang,
+        <Message
+          text="O que é GoLang e quais são suas principais vantagens em comparação com
+      outras linguagens de programação como Python, Java ou C++?"
+          amountOfReactions={182}
+          answered
+        />
+        <Message
+          text="Como funcionam as goroutines em GoLang e por que elas são importantes
+          para a concorrência e paralelismo?"
+          amountOfReactions={173}
+        />
+        <Message
+          text="Quais são as melhores práticas para organizar o código em um projeto
+          GoLang, incluindo pacotes, módulos e a estrutura de diretórios?"
+          amountOfReactions={87}
+        />
+        <Message
+          text="Como fazer a depuração de programas GoLang e quais ferramentas são
+          recomendadas para isso?"
+          amountOfReactions={42}
+        />
+        <Message
+          text="Quais são os primeiros passos para começar a programar em GoLang,
           incluindo a instalação do ambiente de desenvolvimento, configuração e
-          execução do primeiro programa?
-          <button
-            type="button"
-            className="mt-3  flex items-center gap-2 text-orange-400 text-sm font-medium hover:text-orange-500"
-          >
-            <ArrowUp className="size-4" />
-            Curtir pergunta (182)
-          </button>
-        </li>
-        <li className="ml-4 leading-relaxed text-zinc-100">
-          Como funciona o gerenciamento de memória em GoLang, incluindo a coleta
+          execução do primeiro programa?"
+          amountOfReactions={13}
+        />
+        <Message
+          text="Como funciona o gerenciamento de memória em GoLang, incluindo a coleta
           de lixo (garbage collection)? Quais são as implicações de desempenho e
           como otimizar o uso de memória em programas Go? Quais são as
           diferenças entre alocação na stack e no heap, e como essas diferenças
-          afetam a eficiência do programa?
-          <button
-            type="button"
-            className="mt-3  flex items-center gap-2 text-zinc-400 text-sm font-medium hover:text-zinc-300"
-          >
-            <ArrowUp className="size-4" />
-            Curtir pergunta (123)
-          </button>
-        </li>
+          afetam a eficiência do programa?"
+          amountOfReactions={4}
+        />
       </ol>
     </div>
   );
