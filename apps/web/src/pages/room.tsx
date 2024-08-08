17b81e7daf73b97f35f45f7acae5570b66fc456a
Victor Guedes <victorfelipe.guedes@hotmail.com>
2024-08-08 15:56:51 -0300

    feat(web): add create message request

--- apps/web/src/pages/room.tsx
+++ apps/web/src/pages/room.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
 import { useParams } from "react-router-dom";
-import { ArrowRight, Share2 } from "lucide-react";
+import { Share2 } from "lucide-react";
 import { toast } from "sonner";
 
 import { Messages } from "../components/messages";
+import { CreateMessageForm } from "../components/create-message-form";
 
 export function Room() {
   const { roomId } = useParams();
@@ -40,23 +41,7 @@ export function Room() {
 
       <div className="h-px w-full bg-zinc-900" />
 
-      <form className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1">
-        <input
-          type="text"
-          name="theme"
-          placeholder="Qual a sua pergunta?"
-          autoComplete="off"
-          className="flex-1 text-sm bg-transparent mx-2 outline-none placeholder:text-zinc-500 text-zinc-100"
-        />
-
-        <button
-          type="submit"
-          className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm hover:bg-orange-500 transition-colors"
-        >
-          Criar pergunta
-          <ArrowRight className="size-4" />
-        </button>
-      </form>
+      <CreateMessageForm />
 
       <Suspense fallback={<p>Carregando...</p>}>
         <Messages />
